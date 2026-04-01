4d37ab028669422a2bcb0e5060bf603aadac1368
Kirill Egorov <cs15877@gmail.com>
2026-04-01 16:35:18 +0300

    Lesson3_01_about_tuple

---
+++ IntroToSeriesMethods.py
@@ -0,0 +1,13 @@
+import pandas as pd
+price = pd.Series([2.99, 4.45, 1.36])
+price_sum = price.sum() #сумма чисел
+price_product = price.product() #произведение чисел
+price_avg = price.mean() #среднее
+price_std = price.std() # стандартное отклонение
+
+print(price)
+print(price_sum)
+print(price_product)
+print(price_avg)
+print(price_std)
+
