a3449a8e62c7c70bf5298e4289bad1591af5f4b3
Kirill Egorov <cs15877@gmail.com>
2026-04-02 20:17:41 +0300

    Lesson3_01_about_tuple

---
+++ chapter_03/04_List_append_pop.py
@@ -0,0 +1,11 @@
+b_list = ['foo', 'peekaboo', 'baz']
+b_list.append('dwarf') #добавление элемент в конце списка
+print(b_list)
+b_list.insert(1, 'red') #добавление элемент в указанную позицию списка
+print(b_list)
+b_list.pop(2) #удаление элемент с указанной позиции списка
+print(b_list)
+b_list.append('foo2')
+print(b_list)
+b_list.remove('foo2')
+print(b_list)
